e5bc31ea001a3df7fb04994bb6ceed6364db5263
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2018-07-23 00:00:00 +0000

    amd64: add IP to printed register state
    Change-Id: I04113674820a7f6deb852cd45861350e011af05b

--- server/src/ARCH-amd64/cpu_dev.h
+++ server/src/ARCH-amd64/cpu_dev.h
@@ -36,12 +36,15 @@ public:
   void show_state_registers(FILE *f)
   {
     l4_vcpu_regs_t regs = _vcpu->r;
-    fprintf(f, "RAX %lx\nRBX %lx\nRCX %lx\nRDX %lx\nRSI %lx\nRDI %lx\n"
-               "RSP %lx\nRBP %lx\nR8 %lx\nR9 %lx\nR10 %lx\nR11 %lx\n"
-               "R12 %lx\nR13 %lx\nR14 %lx\nR15 %lx\n",
-               regs.ax, regs.bx, regs.cx, regs.dx, regs.si, regs.di, regs.sp,
-               regs.bp, regs.r8, regs.r9, regs.r10, regs.r11, regs.r12,
-               regs.r13, regs.r14, regs.r15);
+    auto *vms = _vcpu.vm_state();
+
+    fprintf(f,
+            "RAX %lx\nRBX %lx\nRCX %lx\nRDX %lx\nRSI %lx\nRDI %lx\n"
+            "RSP %lx\nRBP %lx\nR8 %lx\nR9 %lx\nR10 %lx\nR11 %lx\n"
+            "R12 %lx\nR13 %lx\nR14 %lx\nR15 %lx\nRIP %lx\n",
+            regs.ax, regs.bx, regs.cx, regs.dx, regs.si, regs.di, regs.sp,
+            regs.bp, regs.r8, regs.r9, regs.r10, regs.r11, regs.r12, regs.r13,
+            regs.r14, regs.r15, vms->ip());
   }
 
   /**
