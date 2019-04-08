6ca127261a52b25e7534fe03ee0701ba923516f3
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2019-04-08 00:00:00 +0000

    amd64: reflect UD2 exceptions to the guest
    Change-Id: I17b7c673f56c7e6792b06b9f0eafefaefd1a801a

--- server/src/ARCH-amd64/vm_state_vmx.h
+++ server/src/ARCH-amd64/vm_state_vmx.h
@@ -105,7 +105,8 @@ public:
   {
     vmx_write(L4VCPU_VMCS_LINK_POINTER, 0xffffffffffffffffULL);
     vmx_write(L4VCPU_VMCS_GUEST_ACTIVITY_STATE, 0);
-    vmx_write(L4VCPU_VMCS_EXCEPTION_BITMAP, 0xffffffff &~ (1<<14));
+    // don't exit on PF and UD2
+    vmx_write(L4VCPU_VMCS_EXCEPTION_BITMAP, 0xffffffff & ~(1 << 14 | 1 << 6));
 
     vmx_write(L4VCPU_VMCS_VM_ENTRY_CTLS,
               vmx_read(L4VCPU_VMCS_VM_ENTRY_CTLS) | Vm_entry_load_ia32_efer);
