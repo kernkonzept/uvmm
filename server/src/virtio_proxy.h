279c184260ff73e53188c6fa37da668bd1ea3ed7
Adam Lackorzynski <adam@l4re.org>
2019-05-13 00:00:00 +0000

    virtio_proxy: Fix error message
    Fix message
    VMM: FATAL: Registering RAM for virtio proxy.: Invalid argument
    removing the dot.
    
    Change-Id: I634bf94d3efc9ad1eba034c44a3d12580b2da645

--- server/src/virtio_proxy.h
+++ server/src/virtio_proxy.h
@@ -239,7 +239,7 @@ public:
       {
         L4Re::chksys(_dev.register_ds(r.ds(), r.ds_offset(), r.size(),
                                       r.vm_start().get()),
-                     "Registering RAM for virtio proxy.");
+                     "Registering RAM for virtio proxy");
       });
   }
 
