aeb7263f77f237d312519a57a571c4a26a8df4cc
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2019-11-25 00:00:00 +0000

    Add delivery modes and use RW bitfield member.
    This allows to not only parse MSI-X address and data, but also to create
    MSI messages for internal interfaces, e.g. between the PIC and the local
    APIC on amd64.
    
    Change-Id: I6add254741ea2da23424d64da2d140a65aaf8dda

--- server/src/msi.h
+++ server/src/msi.h
@@ -13,6 +13,7 @@
 
 #include <l4/sys/icu>
 #include <l4/cxx/static_vector>
+#include <l4/cxx/bitfield>
 
 namespace Vdev { namespace Msix {
 
@@ -32,15 +33,25 @@ enum
 struct Interrupt_request_compat
 {
   l4_uint64_t raw;
-  CXX_BITFIELD_MEMBER_RO(32, 63, reserved0_2, raw);
-  CXX_BITFIELD_MEMBER_RO(20, 31, fixed, raw);
-  CXX_BITFIELD_MEMBER_RO(12, 19, dest_id, raw);
-  CXX_BITFIELD_MEMBER_RO(4, 11, reserved0_1, raw);
-  CXX_BITFIELD_MEMBER_RO(3, 3, redirect_hint, raw);
-  CXX_BITFIELD_MEMBER_RO(2, 2, dest_mode, raw);
-  CXX_BITFIELD_MEMBER_RO(0, 1, reserved_0, raw);
+  CXX_BITFIELD_MEMBER(32, 63, reserved0_2, raw);
+  CXX_BITFIELD_MEMBER(20, 31, fixed, raw);
+  CXX_BITFIELD_MEMBER(12, 19, dest_id, raw);
+  CXX_BITFIELD_MEMBER(4, 11, reserved0_1, raw);
+  CXX_BITFIELD_MEMBER(3, 3, redirect_hint, raw);
+  CXX_BITFIELD_MEMBER(2, 2, dest_mode, raw);
+  CXX_BITFIELD_MEMBER(0, 1, reserved_0, raw);
 
   explicit Interrupt_request_compat(l4_uint64_t addr) : raw(addr) {};
+};
+
+enum Delivery_mode : l4_uint8_t
+{
+  Dm_fixed = 0,
+  Dm_lowest_prio = 1,
+  Dm_smi = 2,
+  Dm_nmi = 4,
+  Dm_init = 5,
+  Dm_extint = 7,
 };
 
 /// MSI-X data format (Intel)
@@ -48,10 +59,10 @@ struct Data_register_format
 {
   // Intel SDM Vol. 3A 10-35, October 2017
   l4_uint32_t raw;
-  CXX_BITFIELD_MEMBER_RO(15, 15, trigger_mode, raw);
-  CXX_BITFIELD_MEMBER_RO(14, 14, trigger_level, raw);
-  CXX_BITFIELD_MEMBER_RO(8, 10, delivery_mode, raw);
-  CXX_BITFIELD_MEMBER_RO(0, 7, vector, raw);
+  CXX_BITFIELD_MEMBER(15, 15, trigger_mode, raw);
+  CXX_BITFIELD_MEMBER(14, 14, trigger_level, raw);
+  CXX_BITFIELD_MEMBER(8, 10, delivery_mode, raw);
+  CXX_BITFIELD_MEMBER(0, 7, vector, raw);
 
   explicit Data_register_format(l4_uint32_t data) : raw(data) {};
 };
