45b0ec3ec1e44487f151d06fb38ce45b932f3a3c
Jean Wolter <jean.wolter@kernkonzept.com>
2020-01-20 00:00:00 +0000

    AArch32: Add access to banked registers
    This change adds access to banked registers in set_gpr and
    get_gpr for AArch32. Note, this implementation assumes the
    Uvmm is running in a PL1 mode.
    
    Change-Id: I40df047c1df75bc5722321680477b9d2a76c684e
    Requires-Change-Id: Idc858df300a9eebb6f1264b4b0957b17bdbd3f49

--- server/src/ARCH-arm/vcpu_ptr.h
+++ server/src/ARCH-arm/vcpu_ptr.h
@@ -56,17 +56,84 @@ public:
   void jump_instruction() const
   { _s->r.ip += 2 << hsr().il(); }
 
+  /**
+   * return true if register `x` is the user mode register for the current
+   * mode.
+   */
+  bool use_ureg(unsigned x) const
+  {
+    // regsiters < 8 are always the user registers
+    if (x < 8)
+      return true;
+
+    // one byte for each (leagal) mode, where a set bit
+    // means register r[x] is the user register
+    static l4_uint8_t const i[] =
+      { 0xff, 0x00, 0x3f, 0x3f,
+        0x00, 0x00, 0x00, 0x3f,
+        0x00, 0x00, 0x00, 0x3f,
+        0x00, 0x00, 0x00, 0xff };
+
+    return i[_s->r.flags & 0x0f] & (1 << (x - 8));
+  }
+
+  /**
+   * return a jump offset to be used for accessing non-user SP and LR
+   */
+  unsigned mode_offs() const
+  {
+    // mode (lower 5bits of flags):
+    // 0x12 -> 0, 0x13 -> 2, 0x17 -> 4, 0x1b -> 6, 0x1f -> 0
+    // all other (non hyp) modes use all user registers, or are illegal
+    return ((_s->r.flags + 1) >> 1) & 0x6;
+  }
+
   l4_umword_t get_gpr(unsigned x) const
   {
     if (L4_UNLIKELY(x > 14))
       return 0;
 
-    switch (x)
+    if (use_ureg(x))
+      switch (x)
+        {
+        case 14: return _s->r.lr;
+        case 13: return _s->r.sp;
+        default: return _s->r.r[x];
+        }
+
+    if (0)
+      printf("SPECIAL GET GPR: m=%2lx x=%u\n", (_s->r.flags & 0x1f), x);
+
+    l4_umword_t res;
+    if ((_s->r.flags & 0x1f) == 0x11) // FIQ
       {
-      case 14: return _s->r.lr;
-      case 13: return _s->r.sp;
-      default: return _s->r.r[x];
+        asm (".arch armv7ve\n"
+             "add pc, pc, %[r]\n"
+             "nop\n"
+             "mrs %[res], R8_fiq \n b 2f\n"
+             "mrs %[res], R9_fiq \n b 2f\n"
+             "mrs %[res], R10_fiq\n b 2f\n"
+             "mrs %[res], R11_fiq\n b 2f\n"
+             "mrs %[res], R12_fiq\n b 2f\n"
+             "mrs %[res], SP_fiq \n b 2f\n"
+             "mrs %[res], LR_fiq \n"
+             "2:\n" : [res]"=r"(res) : [r]"r"((x - 8) * 8));
+        return res;
       }
+
+      asm (".arch armv7ve\n"
+           "add pc, pc, %[r]\n"
+           "nop\n"
+           "mrs %[res], SP_irq \n b 2f\n"
+           "mrs %[res], LR_irq \n b 2f\n"
+           "mrs %[res], SP_svc\n b 2f\n"
+           "mrs %[res], LR_svc\n b 2f\n"
+           "mrs %[res], SP_abt\n b 2f\n"
+           "mrs %[res], LR_abt \n b 2f\n"
+           "mrs %[res], SP_und \n b 2f\n"
+           "mrs %[res], LR_und \n"
+           "2:\n" : [res]"=r"(res) : [r]"r"((x - 13 + mode_offs()) * 8));
+      return res;
   }
 
   void set_gpr(unsigned x, l4_umword_t value) const
@@ -74,12 +141,45 @@ public:
     if (L4_UNLIKELY(x > 14))
       return;
 
-    switch (x)
+    if (use_ureg(x))
+      switch (x)
+        {
+        case 14: _s->r.lr = value; return;
+        case 13: _s->r.sp = value; return;
+        default: _s->r.r[x] = value; return;
+        }
+
+    if (0)
+      printf("SPECIAL SET GPR: m=%2lx x=%u\n", (_s->r.flags & 0x1f), x);
+
+    if ((_s->r.flags & 0x1f) == 0x11) // FIQ
       {
-      case 14: _s->r.lr = value; break;
-      case 13: _s->r.sp = value; break;
-      default: _s->r.r[x] = value; break;
+        asm (".arch armv7ve\n"
+             "add pc, pc, %[r]\n"
+             "nop\n"
+             "msr R8_fiq, %[v] \n b 2f\n"
+             "msr R9_fiq, %[v] \n b 2f\n"
+             "msr R10_fiq, %[v]\n b 2f\n"
+             "msr R11_fiq, %[v]\n b 2f\n"
+             "msr R12_fiq, %[v]\n b 2f\n"
+             "msr SP_fiq, %[v] \n b 2f\n"
+             "msr LR_fiq, %[v] \n"
+             "2:\n" : : [v]"r"(value), [r]"r"((x - 8) * 8));
+        return;
       }
+
+      asm (".arch armv7ve\n"
+           "add pc, pc, %[r]\n"
+           "nop\n"
+           "msr SP_irq, %[v] \n b 2f\n"
+           "msr LR_irq, %[v] \n b 2f\n"
+           "msr SP_svc, %[v]\n b 2f\n"
+           "msr LR_svc, %[v]\n b 2f\n"
+           "msr SP_abt, %[v]\n b 2f\n"
+           "msr LR_abt, %[v] \n b 2f\n"
+           "msr SP_und, %[v] \n b 2f\n"
+           "msr LR_und, %[v] \n"
+           "2:\n" : : [v]"r"(value), [r]"r"((x - 13 + mode_offs()) * 8));
   }
 
   l4_umword_t get_lr() const
