a68e077047a59147edfbf6052382e4500258b478
Nicolas Frinker <nicolas_frinker@genua.de>
2022-11-14 00:00:00 +0000

    Implement IOAPIC
    Implement basic reading/writing IOAPIC registers.
    This allows the guest to set up redirection tables and
    makes the IOAPIC a valid choice for an interrupt handler.
    
    This also changes the interrupt handler for uart to
    IOAPIC. The PIC does not work with OpenBSD here, as
    OpenBSD defaults to IOAPIC as soon as it is available.
    
    Tested with Linux and OpenBSD.
    
    Change-Id: If6b9161c90140fbe78b78028a4f438b2152aaf3b

--- server/src/ARCH-amd64/virt_lapic.h
+++ server/src/ARCH-amd64/virt_lapic.h
@@ -833,10 +833,10 @@ private:
 }; // class Msix_control
 
 /**
- * IO-APIC stub. WIP!
+ * Virtual IOAPIC implementation of a 82093AA.
  *
  * TODO The Ic interface is a bit off, as there is no way to clear an IRQ, as
- * the IO-APIC sends an MSI to the MSI-controller when a device sends a legacy
+ * the IOAPIC sends an MSI to the MSI-controller when a device sends a legacy
  * IRQ.
  *  set: send an MSI instead of the legacy IRQ (programmed by OS)
  *  clear: nop
@@ -849,8 +849,53 @@ class Io_apic : public Ic, public Vmm::Mmio_device_t<Io_apic>
 {
   enum
   {
+    Io_apic_id = 0,
+    Io_apic_id_offset = 24,
+    Io_apic_ver = 0x11,
+    Io_apic_num_pins = 24,
     Io_apic_mem_size = 0x1000,
     Irq_cells = 1, // keep in sync with virt-pc.dts
+  };
+
+  enum Ioapic_mmio_regs
+  {
+    Ioregsel = 0,
+    Iowin = 0x10,
+  };
+
+  enum Ioapic_regs
+  {
+    Id_reg = 0,
+    Version_reg = 1,
+    Arbitration_reg = 2,
+    Redir_tbl_offset_reg = 0x10,
+  };
+
+  struct Redir_tbl_entry
+  {
+    enum
+    {
+      Delivery_status_bit = 12,
+      Remote_irr_bit = 14,
+      Ro_mask = 1U << Delivery_status_bit | 1U << Remote_irr_bit,
+    };
+
+    Redir_tbl_entry() noexcept = default;
+    l4_uint64_t raw = 1ULL << 48;
+
+    CXX_BITFIELD_MEMBER_RO(56, 63, dest_id, raw);
+    CXX_BITFIELD_MEMBER_RO(16, 16, masked, raw);
+    CXX_BITFIELD_MEMBER_RO(15, 15, trigger_mode, raw);
+    CXX_BITFIELD_MEMBER_RO(14, 14, remote_irr, raw);
+    CXX_BITFIELD_MEMBER_RO(13, 13, pin_polarity, raw);
+    CXX_BITFIELD_MEMBER_RO(12, 12, delivery_status, raw);
+    CXX_BITFIELD_MEMBER_RO(11, 11, dest_mode, raw);
+    CXX_BITFIELD_MEMBER_RO(8, 10, delivery_mode, raw);
+    CXX_BITFIELD_MEMBER_RO(0, 7, vector, raw);
+
+    // Redirection Table entries can only be written as DWORD.
+    CXX_BITFIELD_MEMBER(0, 31, lower_reg, raw);
+    CXX_BITFIELD_MEMBER(32, 63, upper_reg, raw);
   };
 
 public:
@@ -859,32 +904,122 @@ public:
     Mmio_addr = 0xfec00000,
   };
 
-  Io_apic(cxx::Ref_ptr<Lapic_array> apics) : _apics(apics) {}
+  Io_apic(cxx::Ref_ptr<Lapic_array> apics)
+  : _apics(apics), _id(Io_apic_id << Io_apic_id_offset), _ioregsel(0), _iowin(0)
+  {}
 
   // Mmio device interface
-  l4_umword_t read(unsigned reg, char, unsigned cpu_id)
+  l4_uint64_t read(unsigned reg, char, unsigned cpu_id)
   {
-    trace().printf("Unimplemented MMIO read to register %d by CPU %d\n", reg,
-                  cpu_id);
-    return -1;
+    switch (reg)
+      {
+      case Ioregsel:
+        return _ioregsel;
+      case Iowin:
+        switch (_ioregsel)
+          {
+          case Id_reg:
+            return _id;
+          case Version_reg:
+            return Io_apic_ver | ((Io_apic_num_pins - 1) << 16);
+          case Arbitration_reg:
+            return _id;
+          default:
+            {
+              unsigned index = _ioregsel - Redir_tbl_offset_reg;
+              unsigned irq = index / 2;
+              if (irq >= Io_apic_num_pins)
+                {
+                  warn().printf("Unimplemented MMIO read from ioregsel register 0x%x\n",
+                                _ioregsel.load());
+                  return -1;
+                }
+
+              if (index % 2)
+                return _redirect_tbl[irq].load().upper_reg();
+              else
+                return _redirect_tbl[irq].load().lower_reg();
+            }
+          }
+        break;
+      default:
+        warn().printf("Unimplemented MMIO read from register %d by CPU %d\n",
+                      reg, cpu_id);
+        return -1;
+      }
   }
 
-  void write(unsigned reg, char, l4_umword_t, unsigned cpu_id)
+  void write(unsigned reg, char, l4_uint64_t value, unsigned cpu_id)
   {
-    trace().printf("Unimplemented MMIO write to register %d by CPU %d\n", reg,
-                  cpu_id);
+    switch (reg)
+      {
+      case Ioregsel:
+        _ioregsel = value & 0xff;
+        break;
+      case Iowin:
+        {
+          if (_ioregsel == Id_reg)
+            {
+              _id = value;
+              break;
+            }
+
+          unsigned index = _ioregsel - Redir_tbl_offset_reg;
+          unsigned irq = index / 2;
+          if (irq >= Io_apic_num_pins)
+            {
+              warn().printf("Unimplemented MMIO write to ioregsel register 0x%x\n",
+                           _ioregsel.load());
+              break;
+            }
+
+          Redir_tbl_entry e = _redirect_tbl[irq];
+          if (index % 2)
+            e.upper_reg() = value;
+          else
+            {
+              // ignore writes to RO fields
+              value = (value & ~Redir_tbl_entry::Ro_mask)
+                | e.delivery_status() | e.remote_irr();
+              e.lower_reg() = value;
+            }
+
+          _redirect_tbl[irq] = e; // atomic store
+          break;
+        }
+      default:
+        warn().printf("Unimplemented MMIO write to register %d by CPU %d\n",
+                      reg, cpu_id);
+        break;
+      }
   }
 
   // IC interface
-  void set(unsigned irq) override { _apics->get(0)->set(irq); }
+  void set(unsigned irq) override
+  {
+    Redir_tbl_entry e = redirect(irq);
 
-  void clear(unsigned irq) override { _apics->get(0)->clear(irq); }
+    if (!e.masked())
+      get_apic(e)->set(e.vector());
+  }
+
+  void clear(unsigned irq) override
+  {
+    Redir_tbl_entry e = redirect(irq);
+    get_apic(e)->clear(e.vector());
+  }
 
   void bind_eoi_handler(unsigned irq, Eoi_handler *handler) override
-  { _apics->get(0)->bind_eoi_handler(irq, handler); }
+  {
+    Redir_tbl_entry e = redirect(irq);
+    get_apic(e)->bind_eoi_handler(e.vector(), handler);
+  }
 
   Eoi_handler *get_eoi_handler(unsigned irq) const override
-  { return _apics->get(0)->get_eoi_handler(irq); }
+  {
+    Redir_tbl_entry e = redirect(irq);
+    return get_apic(e)->get_eoi_handler(e.vector());
+  }
 
   int dt_get_interrupt(fdt32_t const *prop, int propsz,
                        int *read) const override
@@ -906,11 +1041,34 @@ public:
 
 
 private:
-  static Dbg trace() { return Dbg(Dbg::Irq, Dbg::Trace, "IO-APIC"); }
-  static Dbg info() { return Dbg(Dbg::Irq, Dbg::Info, "IO-APIC"); }
-  static Dbg warn() { return Dbg(Dbg::Irq, Dbg::Warn, "IO-APIC"); }
+  static Dbg trace() { return Dbg(Dbg::Irq, Dbg::Trace, "IOAPIC"); }
+  static Dbg info() { return Dbg(Dbg::Irq, Dbg::Info, "IOAPIC"); }
+  static Dbg warn() { return Dbg(Dbg::Irq, Dbg::Warn, "IOAPIC"); }
+
+  /**
+   * Get destination APIC for given IRQ
+   * Defaults to APIC 0.
+   */
+  cxx::Ref_ptr<Virt_lapic> get_apic(Redir_tbl_entry e) const
+  {
+    if (auto lapic = _apics->get(e.dest_id() - 1))
+      return lapic;
+    else
+      return _apics->get(0);
+  }
+
+  /// Return the redirection table entry for given `irq`.
+  Redir_tbl_entry redirect(unsigned irq) const
+  {
+    assert(irq < Io_apic_num_pins);
+    return _redirect_tbl[irq];
+  }
 
   cxx::Ref_ptr<Lapic_array> _apics;
+  std::atomic<l4_uint32_t> _id;
+  std::atomic<l4_uint32_t> _ioregsel;
+  std::atomic<l4_uint32_t> _iowin;
+  std::atomic<Redir_tbl_entry> _redirect_tbl[Io_apic_num_pins];
 }; // class Io_apic
 
 } // namespace Gic
