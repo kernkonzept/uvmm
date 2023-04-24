0e0f5df7c5296a3a35071e1e918fb318240014bc
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2023-04-24 00:00:00 +0000

    amd64/pit: refactor to support latch and read-back commands
    The latch and read-back commands need support in each channel/counter to
    transparently read the latch instead of the current count, if there is a
    latched value.  The existing implementation cannot support this
    transparent read operation.
    
    The refactoring introduces the latches for counter and status to each
    channel, as well as support for latch and read-back commands.
    The refactoring moves most of the logic into the Channel abstraction
    which leads to a simplified Pit_timer implementation of io_in/io_out.
    
    Also, fix a bug where also channel 2 emits IRQ0 when reaching a count of
    zero.
    
    Change-Id: I38ebc03205fd96e44c43e6cdc99f4c7028c698cf

--- server/src/ARCH-amd64/pit.cc
+++ server/src/ARCH-amd64/pit.cc
@@ -6,7 +6,6 @@
  * License, version 2.  Please see the COPYING-GPL-2 file for details.
  */
 #include <l4/re/error_helper>
-#include <l4/re/env.h> // l4re_kip
 
 #include "irq_dt.h"
 #include "pit.h"
@@ -14,22 +13,10 @@
 namespace Vdev {
 
 Pit_timer::Pit_timer(cxx::Ref_ptr<Gic::Ic> const &ic, int irq)
-: _irq(ic, irq),
-  _read_high(false), _wait_for_high_byte(false),
-  _port61(make_device<Port61>())
+: _irq(ic, irq), _port61(make_device<Port61>())
 {
   _channel[0] = cxx::make_unique_ptr<Channel>(new Channel(this));
   _channel[1] = cxx::make_unique_ptr<Channel>(new Channel(this, true));
-}
-
-void Pit_timer::set_high_byte(l4_uint16_t &reg, l4_uint8_t value)
-{
-  reg = (reg & Low_byte_mask) | (value << High_byte_shift);
-}
-
-void Pit_timer::set_low_byte(l4_uint16_t &reg, l4_uint8_t value)
-{
-  reg = (reg & High_byte_mask) | value;
 }
 
 void Pit_timer::io_out(unsigned port, Vmm::Mem_access::Width width,
@@ -41,75 +28,57 @@ void Pit_timer::io_out(unsigned port, Vmm::Mem_access::Width width,
   std::lock_guard<std::mutex> lock(_mutex);
 
   switch (port)
-  {
-    case Mode_command : // PIC_MODE
     {
-      _control_reg.raw = value;
-      if (_control_reg.channel() == 1)
-        {
-          warn().printf("set mode for channel 1 unsupported\n");
-          return;
-        }
-      int ch = _control_reg.channel() >> 1;
+    case Mode_command: // PIC_MODE
+      {
+        Control_reg control_reg(value);
+        unsigned channel_select = control_reg.channel();
+        if (channel_select == 1)
+          {
+            warn().printf("set mode for channel 1 unsupported\n");
+            break;
+          }
+        // select either channel 0 or 2
+        channel_select = (channel_select >> 1) & 0x1;
 
-      if (   is_latch_count_value_cmd(_control_reg)
-          || (   _control_reg.channel() == Read_back_cmd
-              && _control_reg.access() == Read_back_latch_cnt))
-        {
-          // We don't emulate the latch register
-          warn().printf("UNIMPLEMENTED: latch register programmed.\n");
-          break;
-        }
-      else
-        _channel[ch]->reset_op_mode();
+        if (control_reg.is_read_back_cmd())
+          {
+            // read-back command
+            if (control_reg.raw & (1U << 1)) // channel 0
+              {
+                if (control_reg.is_latch_status())
+                  _channel[0]->latch_status();
+                if (control_reg.is_latch_count())
+                  _channel[0]->latch_count();
+              }
+            if (control_reg.raw & (1U << 3)) // channel 2
+              {
+                if (control_reg.is_latch_status())
+                  _channel[2]->latch_status();
+                if (control_reg.is_latch_count())
+                  _channel[2]->latch_count();
+              }
+            trace().printf("Read-back command: 0x%x\n", control_reg.raw);
+            break;
+          }
 
-      _channel[ch]->op_mode(static_cast<Channel::Mode>(_control_reg.opmode().get()));
-      trace().printf("New timer mode on channel %d: 0x%x\n",
-                     ch, _control_reg.opmode().get());
-      break;
-    }
+        _channel[channel_select]->write_status(control_reg.raw
+                                                            & 0x3f);
+        trace().printf("Mode command on channel %d: 0x%x\n", channel_select,
+                       control_reg.raw);
+        break;
+      }
     case Channel_0_data:
     case Channel_2_data:
-      trace().printf("Writing 0x%x for channel %d\n", value,
-                     port);
-      if (!is_latch_count_value_cmd(_control_reg)
-          && is_current_channel(_control_reg, port))
-        {
-          unsigned ch = port2idx(port);
-          unsigned v = value & 0xFF;
+      {
+        trace().printf("Writing 0x%x for channel %d\n", value, port);
 
-          if (_control_reg.access() == Access_lohi)
-            {
-              if (_wait_for_high_byte)
-                {
-                  set_high_byte(_reload, v);
-                  _wait_for_high_byte = false;
-                  _channel[ch]->reset(_reload);
-                  _channel[ch]->enable_irq();
-                }
-              else
-                {
-                  // lobyte first
-                  set_low_byte(_reload, v);
-                  _channel[ch]->disable_irq();
-                  // wait for sequential write to high byte
-                  _wait_for_high_byte = true;
-                  return;
-                }
-            }
-          else if (_control_reg.access() == Access_lobyte)
-            set_low_byte(_reload, v);
-          else if (_control_reg.access() == Access_hibyte)
-            {
-              set_high_byte(_reload, v);
-              _channel[ch]->reset(_reload);
-              _channel[ch]->enable_irq();
-            }
-
-          trace().printf("set counter for %d to %d\n", ch, _reload);
-        }
-      else
-        warn().printf("PIT access to bad channel\n");
+        unsigned channel_select = port2idx(port);
+        _channel[channel_select]->write_count(value & 0xff);
+        break;
+      }
+    default:
+      warn().printf("write to unimplemented channel 1\n");
       break;
   }
 }
@@ -126,31 +95,110 @@ void Pit_timer::io_in(unsigned port, Vmm::Mem_access::Width width,
 
   switch (port)
     {
-    case Mode_command: *value = _control_reg.raw; break;
+    case Mode_command: /* Register is write only. Ignore read. */ break;
 
     case Channel_0_data:
     case Channel_2_data:
       {
-        l4_uint16_t reg;
-        int ch = port2idx(port);
-
+        unsigned ch = port2idx(port);
         std::lock_guard<std::mutex> lock(_mutex);
-        reg = _channel[ch]->current();
-
-        switch (_control_reg.access())
-          {
-          case Access_lobyte: *value = reg & 0xff; break;
-          case Access_hibyte: *value = (reg >> 8) & 0xff; break;
-          case Access_lohi:
-            *value = _read_high ? (reg >> 8) : (reg & 0xFF);
-            _read_high = !_read_high;
-            break;
-          default:
-            warn().printf("Invalid access mode during read: Mode: 0x%x\n",
-                          _control_reg.raw);
-            break;
-          }
+        *value = _channel[ch]->read();
+        break;
       }
+    default:
+      warn().printf("PIT read from unimplemented channel 1\n");
+      break;
+    }
+}
+
+void Pit_timer::Channel::write_count(l4_uint8_t value)
+{
+  _count_latch.reset();
+  _status_latch.reset();
+  stop_counter();
+
+  switch(_status.access())
+    {
+    case Access_lobyte:
+      _reload = set_low_byte(_reload, value);
+      start_counter();
+      break;
+    case Access_hibyte:
+      _reload = set_high_byte(_reload, value);
+      start_counter();
+      break;
+    case Access_lohi:
+      write_lo_hi(value);
+      break;
+    default:
+      warn().printf("Invalid access value for write to counter: counter "
+                    "%u, status 0x%x\n",
+                    _is_channel2 ? 2U : 0U, _status.raw);
+      break;
+    }
+}
+
+void Pit_timer::Channel::write_status(l4_uint8_t value)
+{
+  if ((value & 0x30U) == 0) // latch command
+    {
+      latch_count();
+      return;
+    }
+
+  // Spec states: When writing to control word, all control logic resets.
+  stop_counter();
+  _count_latch.reset();
+  _status_latch.reset();
+  _read_lo = true;
+  _write_lo = true;
+
+  _status.write(value);
+  trace().printf("New status on channel: 0x%x\n", _status.raw);
+}
+
+l4_uint8_t Pit_timer::Channel::read()
+{
+  if (_status_latch.valid)
+    {
+      _status_latch.valid = false;
+      return _status_latch.value & 0xff;
+    }
+
+  if (_count_latch.valid)
+    {
+      switch (_status.access())
+        {
+        case Access_lobyte:
+          _count_latch.valid = false;
+          return low_byte(_count_latch.value);
+        case Access_hibyte:
+          _count_latch.valid = false;
+          return high_byte(_count_latch.value);
+        case Access_lohi:
+          if (_count_latch.read_lo == false) // reading 2nd byte invalidates
+            _count_latch.valid = false;
+          return read_lo_hi(&_count_latch.read_lo, _count_latch.value);
+        default:
+          warn().printf("Read latch with invalid access mode: counter "
+                        "%u, status 0x%x\n",
+                        _is_channel2 ? 2U : 0U, _status.raw);
+          return 0;
+        }
+    }
+
+  // read counter
+  l4_uint16_t curr = current();
+  switch (_status.access())
+    {
+    case Access_lobyte: return low_byte(curr);
+    case Access_hibyte: return high_byte(curr);
+    case Access_lohi: return read_lo_hi(&_read_lo, curr);
+    default:
+      warn().printf("Read counter with invalid access mode: counter "
+                    "%u, status 0x%x\n",
+                    _is_channel2 ? 2U : 0U, _status.raw);
+      return 0;
     }
 }
 
--- server/src/ARCH-amd64/pit.h
+++ server/src/ARCH-amd64/pit.h
@@ -10,6 +10,7 @@
 #include <mutex>
 #include <l4/cxx/bitfield>
 #include <l4/cxx/unique_ptr>
+#include <l4/re/env.h>
 
 #include "device.h"
 #include "io_device.h"
@@ -22,6 +23,8 @@ namespace Vdev {
  * Limited implementation of 8254 PROGRAMMABLE INTERVAL TIMER.
  *
  * Supports only channel 0 and 2.
+ * After a read-back command with status field, the following bits in the
+ * status field latched are not supported: OUTPUT [7], NULL COUNT [6].
  *
  * \note This timer model uses the KIP clock as time base. You need to
  *       configure the Microkernel with CONFIG_SYNC_TSC in order to achieve
@@ -63,10 +66,7 @@ class Pit_timer
     Latch_cmd_null_mask = 0x3f,
     Latch_cmd_channel_mask = 0xc0,
 
-    Read_back_cmd = 3,
-    Read_back_latch_0 = 1,
-    Read_back_latch_2 = (1 << 2),
-    Read_back_latch_cnt = 2,
+    Access_latch = 0,
     Access_lobyte = 1,
     Access_hibyte = 2,
     Access_lohi = 3,
@@ -74,19 +74,145 @@ class Pit_timer
 
   class Channel: public L4::Ipc_svr::Timeout_queue::Timeout
   {
-  public:
-    enum Mode : l4_uint8_t
+    struct Status
     {
-      Mode_terminal_count = 0x0,
-      Mode_retriggerable_one_shot = 0x1,
-      Mode_rate_generator = 0x2,
-      Mode_disabled = 0xff,
+      Status() : raw(0) {}
+      Status(l4_uint8_t v) : raw(v) {}
+
+      l4_uint8_t raw = 0;
+      CXX_BITFIELD_MEMBER(7, 7, output, raw);
+      CXX_BITFIELD_MEMBER(6, 6, count, raw);
+      CXX_BITFIELD_MEMBER(4, 5, access, raw);
+      CXX_BITFIELD_MEMBER(1, 3, opmode, raw);
+      CXX_BITFIELD_MEMBER(0, 0, bcd, raw);
+
+      enum
+      {
+        // Bits not changed on mode command
+        Retain_mask = output_bfm_t::Mask | count_bfm_t::Mask
+      };
+
+      void write(l4_uint8_t val)
+      { raw = (val & ~Retain_mask) | (raw & Retain_mask); }
     };
 
+    struct Latch
+    {
+      void reset()
+      {
+        value = 0;
+        valid = false;
+        read_lo = true;
+      }
+
+      l4_uint16_t value = 0;
+      bool valid = false;
+      bool read_lo = true;
+    };
+
+  public:
     Channel(Pit_timer *pit, bool is_channel2 = false)
-    : _reload(0), _op_mode(Mode_disabled), _irq_on(false), _pit(pit),
-      _is_channel2(is_channel2), _reload_kip_clock(0)
+    : _is_channel2(is_channel2), _pit(pit)
     {}
+
+    // called in the context of the timer thread, be careful with locking!
+    void expired()
+    {
+      if (_is_channel2 && _pit->_port61->channel_2_on())
+        _pit->_port61->set_output();
+
+      if (!_is_channel2)
+        _pit->_irq.inject();
+
+      // only rate generator mode is periodic
+      if (_status.opmode() == 0x2) // Mode: rate generator
+        {
+          _pit->requeue_timeout(this, next_timeout_us());
+          _reload_kip_clock = l4_kip_clock(l4re_kip());
+        }
+    }
+
+    void latch_count()
+    {
+      // ignore all but the first latch command
+      if (_count_latch.valid)
+        return;
+
+      _count_latch.value = current();
+      _count_latch.valid = true;
+      _count_latch.read_lo = true;
+    }
+
+    void latch_status()
+    {
+      if (_status_latch.valid)
+        return;
+
+      _status_latch.value = _status.raw;
+      _status_latch.valid = true;
+    }
+
+    void write_count(l4_uint8_t value);
+    void write_status(l4_uint8_t value);
+    l4_uint8_t read();
+
+  private:
+    static l4_uint8_t low_byte(l4_uint16_t v)
+    { return v & Low_byte_mask; }
+
+    static l4_uint8_t high_byte(l4_uint16_t v)
+    { return (v >> High_byte_shift) & Low_byte_mask; }
+
+    static l4_uint16_t set_high_byte(l4_uint16_t reg, l4_uint8_t value)
+    { return (reg & Low_byte_mask) | (value << High_byte_shift); }
+
+    static l4_uint16_t set_low_byte(l4_uint16_t reg, l4_uint8_t value)
+    { return (reg & High_byte_mask) | value; }
+
+    static l4_uint8_t read_lo_hi(bool *read_lo, l4_uint16_t count)
+    {
+      l4_uint8_t ret = 0;
+      if (*read_lo)
+          ret = low_byte(count);
+      else
+          ret = high_byte(count);
+
+      *read_lo = !*read_lo;
+      return ret;
+    }
+
+    void write_lo_hi(l4_uint8_t value)
+    {
+      if (_write_lo)
+        _reload = set_low_byte(_reload, value);
+      else
+        {
+          _reload = set_high_byte(_reload, value);
+          start_counter();
+        }
+
+      _write_lo = !_write_lo;
+    }
+
+    void disable_irq()
+    { _pit->dequeue_timeout(this); }
+
+    void enable_irq()
+    {
+      if (_reload)
+        _pit->enqueue_timeout(this, next_timeout_us());
+    }
+
+    void start_counter()
+    {
+      _reload_kip_clock = l4_kip_clock(l4re_kip());
+      enable_irq();
+    }
+
+    void stop_counter()
+    {
+      disable_irq();
+    }
 
     /**
      * Next absolute timeout in microseconds.
@@ -99,58 +225,6 @@ class Pit_timer
       l4_cpu_time_t timeout_us =
         _reload * Microseconds_per_second / Pit_tick_rate;
       return kip + timeout_us;
-    }
-
-    // called in the context of the timer thread, be careful with locking!
-    void expired()
-    {
-      if (_is_channel2 && _pit->_port61->channel_2_on())
-        _pit->_port61->set_output();
-
-      _pit->_irq.inject();
-
-      // only rate generator mode is periodic
-      if (_op_mode == Mode_rate_generator)
-        {
-          _pit->requeue_timeout(this, next_timeout_us());
-          _reload_kip_clock = l4_kip_clock(l4re_kip());
-        }
-    }
-
-    /**
-     * Load the counter with new value
-     *
-     * \param new_reload  Value to count down from.
-     */
-    void reset(l4_uint16_t new_reload)
-    {
-      _reload = new_reload;
-      _reload_kip_clock = l4_kip_clock(l4re_kip());
-      disable_irq();
-    }
-
-    void op_mode(Mode m)
-    {
-      assert(m < 8);
-      _op_mode = m;
-      disable_irq();
-    }
-
-    void reset_op_mode()
-    {
-      disable_irq();
-      _op_mode = Mode_disabled;
-    }
-
-    void disable_irq()
-    {
-      _pit->dequeue_timeout(this);
-    }
-
-    void enable_irq()
-    {
-      if (_reload)
-        _pit->enqueue_timeout(this, next_timeout_us());
     }
 
     /**
@@ -177,33 +251,31 @@ class Pit_timer
       return _reload - ticks;
     }
 
-  private:
-    l4_uint16_t _reload;
-    Mode _op_mode;
-    bool _irq_on;
-    Pit_timer *_pit;
+    l4_uint16_t _reload = 0U;
+    Status _status;
     bool _is_channel2;
-    l4_cpu_time_t _reload_kip_clock;
+    Latch _count_latch;
+    Latch _status_latch;
+    Pit_timer *_pit;
+    l4_cpu_time_t _reload_kip_clock = 0ULL;
+    bool _read_lo = true;
+    bool _write_lo = true;
   };
 
   struct Control_reg
   {
+    Control_reg(l4_uint8_t val) : raw(val) {}
+
     l4_uint8_t raw;
     CXX_BITFIELD_MEMBER(6, 7, channel, raw);
     CXX_BITFIELD_MEMBER(4, 5, access, raw);
     CXX_BITFIELD_MEMBER(1, 3, opmode, raw);
     CXX_BITFIELD_MEMBER(0, 0, bcd, raw);
+
+    bool is_read_back_cmd() const { return channel() == 3; }
+    bool is_latch_status() const { return !(raw & (1U << 4)); }
+    bool is_latch_count() const { return !(raw & (1U << 5)); }
   };
-
-  bool is_latch_count_value_cmd(Control_reg m) const
-  {
-    return !(m.raw & Latch_cmd_null_mask);
-  }
-
-  bool is_current_channel(Control_reg m, int port) const
-  {
-    return m.channel() == port;
-  }
 
   static constexpr int port2idx(int port) { return port >> 1; }
 
@@ -223,16 +295,9 @@ public:
 
 private:
   Vmm::Irq_edge_sink _irq;
-  l4_uint16_t _reload;
   cxx::unique_ptr<Channel> _channel[Channels];
-  bool _read_high;
-  bool _wait_for_high_byte;
-  Control_reg _control_reg;
   std::mutex _mutex;
   cxx::Ref_ptr<Port61> const _port61;
-
-  void set_high_byte(l4_uint16_t &reg, l4_uint8_t value);
-  void set_low_byte(l4_uint16_t &reg, l4_uint8_t value);
 };
 
 } // namespace Vdev
