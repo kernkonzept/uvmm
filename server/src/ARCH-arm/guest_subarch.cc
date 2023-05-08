e7ccca4389df82d3e6590ac7a19b56fb39b1b139
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2023-05-08 00:00:00 +0000

    arm: always run threads in EL0 by default
    On a !cpu_virt kernel, threads naturally run in EL0. We now keep the
    same default for cpu_virt kernels too. Because a VMM must have access
    to certain registers in EL1, add the required loader flags to be
    started in EL1.
    
    For arm64 the syscall must be overridden to use the correct
    instruction. The KIP is not usable because it will use the wrong
    instruction.
    
    Change-Id: I2b949d873d541dbe16ebb124e53a258903030dcc

--- server/src/ARCH-arm/guest_subarch.cc
+++ server/src/ARCH-arm/guest_subarch.cc
@@ -4,7 +4,13 @@
  * Author(s): Christian Pötzsch <christian.poetzsch@kernkonzept.com>
  */
 
+#include <l4/sys/thread.h>
+#include <l4/re/elf_aux.h>
 #include "guest.h"
+
+L4RE_ELF_AUX_ELEM_T(l4re_elf_aux_mword_t, __ex_regs_flags,
+                    L4RE_ELF_AUX_T_EX_REGS_FLAGS,
+                    L4_THREAD_EX_REGS_ARM_SET_EL_EL1);
 
 namespace Vmm {
 
