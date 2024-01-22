a0fe216783b09e8769c3a0458f5f84eaca76d830
Steffen Liebergeld <steffen.liebergeld@kernkonzept.com>
2024-01-22 00:00:00 +0000

    uvmm_dtg: add l4rtc support
    Change-Id: I79bdb9250dc8bf1998350d050295f15218ef5f47

--- tools/uvmm_dtg/build/devices/rtc.cc
+++ tools/uvmm_dtg/build/devices/rtc.cc
@@ -16,6 +16,14 @@ struct Rtc: Device
   {
     auto a = dt->root()->add_section("rtc");
     a->add_compatible("virt-rtc");
+    a->add_num_property("interrupts", 8);
+
+    if (_res.has("rtc_cap"))
+      {
+        auto b = dt->root()->add_section("l4rtc");
+        b->add_compatible("l4rtc");
+        b->add_str_property("l4vmm,rtccap", _res.as<std::string>("rtc_cap"));
+      }
   }
 };
 
@@ -24,7 +32,11 @@ struct F: Device_factory<Rtc>
   using Device_factory::Device_factory;
 
   Option option() override
-  { return Device_option("rtc", "rtc device", this); }
+  { return Device_option("rtc", "rtc device", this,
+                         {Option("rtc_cap", "Capability for the l4rtc server",
+                                 make_parser<String_parser>())
+                         });
+  }
 
   int flags() const override
   { return Option::Default; }
