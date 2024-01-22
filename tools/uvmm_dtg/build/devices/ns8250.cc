fd25677de82045b315976a3689cc3d3c10043efc
Steffen Liebergeld <steffen.liebergeld@kernkonzept.com>
2024-01-22 00:00:00 +0000

    uvmm_dtg: make ns8250 irq configurable
    On x86 the com1 irq is 4 and com2 is 3. Those are fixed. Here
    we allow those numbers to be handed in by the user.
    
    Change-Id: I5524b6ba87dd07ec31efa59d3645880e0b8373c7

--- tools/uvmm_dtg/build/devices/ns8250.cc
+++ tools/uvmm_dtg/build/devices/ns8250.cc
@@ -18,14 +18,17 @@ struct Ns8250: Device
     auto t = _trg_arch.is(Arch::X86) ? dt->root() : dt->l4vmm();
     auto a = t->add_section("ns8250");
     a->add_compatible({"ns8250", "uart,8250" });
-    a->add_num_property("interrupts", Ic::default_ic(_trg_arch)->next_irq());
     if (!_trg_arch.is(Arch::X86))
       {
         a->add_reg_property(Addr_type(_res.as<uint64_t>("addr"),
                                       0x1000, dt->rm()));
         a->add_str_property("clock-names", "apb_pclk");
         a->add_handle_property("clocks", "/sysclk");
+        a->add_num_property("interrupts", Ic::default_ic(_trg_arch)->next_irq());
       }
+    else
+      a->add_num_property("interrupts", _res.as<uint8_t>("irqnum"));
+
     if (_res.has("vcon_cap"))
       a->add_str_property("l4vmm,vcon_cap", _res.as<std::string>("vcon_cap"));
   }
@@ -42,6 +45,10 @@ struct F: Device_factory<Ns8250>
       opts.emplace_back("addr", "fixed address of the device mmio region",
                         make_parser<Addr_parser>(_trg_arch.is64bit),
                         make_auto<Addr_default>());
+    else
+      opts.emplace_back("irqnum", "fixed irq number (on x86: com1 = 4, com2 = 3)",
+                       make_parser<UInt8_parser>(),
+                       Option::Required);
 
     opts.emplace_back("vcon_cap", "name of the vcon capability",
                       make_parser<String_parser>());
