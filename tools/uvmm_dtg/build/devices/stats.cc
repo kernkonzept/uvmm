823e1d44e515423183e836288ce1355028c62400
Steffen Liebergeld <steffen.liebergeld@kernkonzept.com>
2024-04-08 00:00:00 +0000

    Uvmm_dtg: add statistics interface
    Change-Id: I5a998fadadff3ce887bb3253e2c4ff7ed1d4e25e

---
+++ tools/uvmm_dtg/build/devices/stats.cc
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2023 Kernkonzept GmbH.
+ * Author(s): Steffen Liebergeld steffen.liebergeld@kernkonzept.com
+ *
+ * License: see LICENSE.spdx (in this directory or the directories above)
+ */
+#include "device.h"
+
+namespace {
+
+struct Stats: Device
+{
+  using Device::Device;
+
+  void add(Tree *dt) override
+  {
+    if (_res.has("statscap"))
+      {
+        auto b = dt->root()->add_section("stats");
+        b->add_compatible("l4vmm,stats");
+        b->add_str_property("l4vmm,statscap", _res.as<std::string>("statscap"));
+      }
+  }
+};
+
+struct F: Device_factory<Stats>
+{
+  using Device_factory::Device_factory;
+
+  Option option() override
+  { return Device_option("stats", "statistics interface", this,
+                         {Option("statscap",
+                                 "Capability for the statistics interface",
+                                 make_parser<String_parser>())
+                         });
+  }
+
+  int flags() const override
+  { return Option::Default; }
+};
+
+static F f = { Arch::X86, "stats" };
+
+}
