674eb058341b04f5bdbf3f9ef56259c7eb475209
Frank Mehnert <frank.mehnert@kernkonzept.com>
2024-04-29 00:00:00 +0000

    fix constant for upper memory limit
    Vmm::Guest_addr uses l4_addr_t to store the address. Use the proper
    constant to prevent a warning on 32-bit systems.
    
    Change-Id: I385bd878d234f54f99bb06161f281efdecc3d801

--- server/src/vm_ram.h
+++ server/src/vm_ram.h
@@ -50,7 +50,7 @@ public:
   bool reserve_fixed(Vmm::Guest_addr start, l4_size_t size);
   bool reserve_back(l4_size_t size, Vmm::Guest_addr *start,
                     unsigned char page_shift = L4_PAGESHIFT,
-                    Vmm::Guest_addr upper_limit = Vmm::Guest_addr(~0ULL));
+                    Vmm::Guest_addr upper_limit = Vmm::Guest_addr(~0UL));
 
   long load_file_to_back(Vm_ram *ram, char const *name,
                          Vmm::Guest_addr *start, l4_size_t *size);
