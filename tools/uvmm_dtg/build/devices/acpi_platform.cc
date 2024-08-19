a81dcba5bef98cfe83c2870663e23da649602316
Steffen Liebergeld <steffen.liebergeld@kernkonzept.com>
2024-08-19 00:00:00 +0000

    uvmm_dtg: add power vcon input option to platform-acpi device
    Change-Id: I24a7c2f7ee138cd402b4c91a24051bc93bb33fa1

--- tools/uvmm_dtg/build/devices/acpi_platform.cc
+++ tools/uvmm_dtg/build/devices/acpi_platform.cc
@@ -21,6 +21,7 @@ struct Acpi_platform: Device
     auto a = dt->root()->add_section("acpi_platform");
     a->add_compatible("virt-acpi");
     a->add_num_property("interrupts", 9);
+    a->add_str_property("l4vmm,pwrinput", _res.as<std::string>("vcon_cap"));
   }
 };
 
@@ -29,7 +30,15 @@ struct F: Device_factory<Acpi_platform>
   using Device_factory::Device_factory;
 
   Option option() override
-  { return Device_option("acpi-platform", "acpi platform device", this); }
+  {
+    std::vector<Option> opts =
+      {Option("vcon_cap", "vcon channel for acpi commands",
+              make_parser<String_parser>(),
+              make_default<std::string>("acpi_pwr_input")),};
+
+    return Device_option("acpi-platform", "acpi platform device", this,
+                         std::move(opts));
+  }
 
   int flags() const override
   { return Option::Default; }
